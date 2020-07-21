302b758ed18949ae2484a4beac2005633856035a
zacharukaz <zacharukaz@gmail.com>
2020-07-21 12:05:55 -0300

    Cadastrando tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,25 +1,29 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController //já assume que to método vai ter o @ResponseBody
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping //DTO são dados que saem da API de volta pro cliente
     public List<TopicoDto> lista(String nomeCurso){
         if (nomeCurso == null){
             List<Topico> topicos = topicoRepository.findAll();
@@ -28,5 +32,11 @@ public class TopicosController {
             List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
             return TopicoDto.converter(topicos);
         }
+    }
+
+    @PostMapping //Form são dados que chegam do cliente para a API
+    public void cadastrar(@RequestBody TopicoForm form){
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
     }
 }
